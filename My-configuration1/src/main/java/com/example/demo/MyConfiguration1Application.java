6cc36627de85d50f117e19ea6941aecaf4efdd74
Jihen Amraoui <Jihen Amraoui@DESKTOP-DMEF8IR>
2018-11-23 22:30:59 +0100

    deuxième config

---
+++ My-configuration1/src/main/java/com/example/demo/MyConfiguration1Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyConfiguration1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyConfiguration1Application.class, args);
+	}
+}
